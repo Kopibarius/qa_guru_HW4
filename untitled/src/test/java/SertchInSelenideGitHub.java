461461a60bcce7862a561fa3357133dfb5f5b4b2
Egor Melnichyk <egor.mel8@gmail.com>
2024-03-26 01:10:03 +0300

    Final commit

---
+++ untitled/src/test/java/SertchInSelenideGitHub.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SertchInSelenideGitHub {
+
+    @Test
+    void serchingJUnit5CodeInSoftAssertions() {
+        open("https://github.com/selenide/selenide/tree/main");
+        $("#wiki-tab").click();
+        $("#wiki-body").shouldHave(text("Soft assertions"));
+        $(By.linkText("Soft assertions")).click();
+        $("#wiki-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+    }
+
+}
+
